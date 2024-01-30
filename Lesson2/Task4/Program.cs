9285f043a59f62d8e3de8bed12f8c55e7242fb95
Alexandr Vinogradov <van16@mail.ru>
2024-01-30 21:57:58 +0300

    add Lesson2:task4

---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 10;
+int[] array = {21,16,42,76,58,95,31,1,28,65};
+int i = 0;
+int max = array[0];
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+System.Console.WriteLine(max);
