ad5091fa20cf40aea694029c522f41a3d71b62c4
Alexandr Vinogradov <van16@mail.ru>
2024-01-30 20:00:23 +0300

    add lesson1:task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 12;
+int b = 23;
+int c = 73;
+int d = 9;
+int e = 48;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
