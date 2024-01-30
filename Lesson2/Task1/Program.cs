c562b71b18fb5a40b1360dbe9e0c141ad45f8628
Alexandr Vinogradov <van16@mail.ru>
2024-01-30 21:12:40 +0300

    add Lesson2:task1

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int [] arr = new int [5];
+arr [0] = 4;
+arr [1] = 91;
+arr [2] = 26;
+arr [3] = 67;
+arr [4] = 14;
+Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[1]);
+System.Console.WriteLine(arr[2]);
+System.Console.WriteLine(arr[3]);
+System.Console.WriteLine(arr[4]);
