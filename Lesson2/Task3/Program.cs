0d4d6bf37e7b257e2115497c14de1c401dcda274
Alexandr Vinogradov <van16@mail.ru>
2024-01-30 21:51:32 +0300

    add Lesson2:task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 10;
+int[] array = {21,16,42,76,58,95,31,1,28,65};
+int i = 0;
+while (i < n)
+{
+    if (array[i] % 2 ==0)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
