1e6ec5fd09af6572d946d3dd129f693da0a79650
AlexandrVinogradov <vanhorn.av@gmail.com>
2024-02-11 20:10:35 +0300

    add

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿string GerLetters(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (IsAsciiLetter(e) == true)
+        {
+            letters += e;
+        }
+    }
+    return letters;
+}
+
+
+
+
+
+
+
+
+
+string str = Console.ReadLine();
+string result = GerLetters(str);
+System.Console.WriteLine(result);
